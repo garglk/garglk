7f121bfec8836cdcbcfb2851310a1f3a606bfe93
bcressey <ben@cressey.org>
2008-11-04 18:55:57 +0000

    Redo: added sources for Jacl 2.3.14.
    svn-id: r73

---
+++ terps/jacl-2.3.14/version.h
@@ -0,0 +1,9 @@
+/* version.h --- Version constants for the JACL interpreters.
+ * (C) 2008 Stuart Allen, distribute and use 
+ * according to GNU GPL, see file COPYING for details.
+ */
+
+#define J_VERSION               2
+#define J_RELEASE               3
+#define	J_BUILD					14
+#define INTERPRETER_VERSION		203
