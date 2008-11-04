e78be2bdb319482ea8787dbd76c74dc540777ec2
bcressey <bcressey@be9d5a54-a115-11dd-a9c7-49dec45ca499>
2008-11-04 18:53:34 +0000

    Removed old sources for Jacl.

--- trunk/terps/jacl/version.h
+++
@@ -1,9 +0,0 @@
-/* version.h --- Version constants for the JACL interpreters.
- * (C) 2008 Stuart Allen, distribute and use 
- * according to GNU GPL, see file COPYING for details.
- */
-
-#define J_VERSION               2
-#define J_RELEASE               2
-#define	J_BUILD					7
-#define INTERPRETER_VERSION		202
