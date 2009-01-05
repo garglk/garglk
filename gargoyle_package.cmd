07d49b2fddb1b42ef110cd034c89742aebee92da
bcressey <ben@cressey.org>
2009-01-05 22:30:02 +0000

    Patch for Jam build scripts: add variables to control install directories.
    svn-id: r155

--- gargoyle_package.cmd
+++ gargoyle_package.cmd
@@ -1,12 +1,6 @@
 @ECHO OFF
 echo Packaging Gargoyle binaries ...
 jam -sOS=MINGW install
-copy support\sdl-1.2.13\SDL_mixer.dll build\dist
-copy support\sdl-1.2.13\SDL.dll build\dist
-copy support\sdl-1.2.13\smpeg.dll build\dist
-copy support\sdl-1.2.13\libogg-0.dll build\dist
-copy support\sdl-1.2.13\libvorbis-0.dll build\dist
-copy support\sdl-1.2.13\libvorbisfile-3.dll build\dist
 makensis.exe installer.nsi
 echo ... finished packaging!
 pause
