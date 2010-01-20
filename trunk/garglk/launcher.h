02deba67c87fbf7ac5011f3f824e4b7592d683de
bcressey <bcressey@be9d5a54-a115-11dd-a9c7-49dec45ca499>
2010-01-20 00:44:18 +0000

    Separated launcher into common code and platform specific functions.

--- trunk/garglk/launcher.h
+++ trunk/garglk/launcher.h
@@ -1,7 +1,8 @@
 /******************************************************************************
  *                                                                            *
+ * Copyright (C) 2006-2009 by Tor Andersson.                                  *
+ * Copyright (C) 2008-2010 by Ben Cressey.                                    *
  * Copyright (C) 2009 by Baltasar GarcÌa Perez-Schofield.                     *
- * Copyright (C) 2010 by Ben Cressey.                                         *
  *                                                                            *
  * This file is part of Gargoyle.                                             *
  *                                                                            *
@@ -28,102 +29,13 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#include "glk.h"
-#include "glkstart.h"
-#include "gi_blorb.h"
-#include "garversion.h"
+extern void wininit(void);
+extern void winmsg(const char *msg);
+extern bool winargs(int argc, char **argv, char *buffer);
+extern void winopenfile(char *buffer);
+extern void winpath(char *buffer);
+extern bool winexec(const char *cmd, char **args);
+extern void winterp(char *path, char *exe, char *flags, char *game);
+extern void runblorb(char *path, char *game);
+extern void rungame(char *path, char *game);
 
-/* Detect OS */
-#ifdef __WIN32__
-	#define OS_WINDOWS
-#else
-#ifdef _Windows	
-	#define OS_WINDOWS
-#else
-#ifdef __Windows
-	#define OS_WINDOWS
-#endif
-#endif
-#endif
-
-#ifdef __linux__
-	#define OS_UNIX
-#else
-#ifdef __unix__
-	#define OS_UNIX
-#endif
-#endif
-
-#ifdef __APPLE__
-	#define OS_MAC
-#endif
-
-/* Check OS detected */
-#ifndef OS_WINDOWS
-#ifndef OS_UNIX
-#ifndef OS_MAC
-	#error "Compilation stopped. No OS found."
-#endif
-#endif
-#endif
-
-/* Include required headers for GUI */
-#ifdef OS_WINDOWS
-	#include <windows.h>
-	#include <commdlg.h>
-#else
-#ifdef OS_UNIX
-	#include <gtk/gtk.h>
-	#include <unistd.h>
-#else
-#ifdef OS_MAC
-	#import <Cocoa/Cocoa.h>
-#endif
-#endif
-#endif
-
-#ifdef OS_MAC
-
-@interface nsDelegate : NSObject
-{
-	NSString * fileref ;
-}
-@end
-
-@implementation nsDelegate
-
-- (id) init
-{
-	fileref = [[NSString alloc] initWithString: @""];
-	return [super init];
-}
-
-- (BOOL) application: (NSApplication *) theApplication openFile: (NSString *) file
-{
-	if (fileref)
-		[fileref release];
-	
-	if ([file length])
-		fileref = [[NSString alloc] initWithString: file];
-}
-
-- (BOOL) application: (NSApplication *) theApplication openFiles: (NSArray *) files
-{
-	if (fileref)
-		[fileref release];
-	
-	if ([files count])
-		fileref = [[NSString alloc] initWithString: [files objectAtIndex: 0]];
-	
-	return YES;
-}
-
-- (NSString *) argv
-{
-	if (fileref)
-		return fileref;
-}
-
-@end
-
-#endif /* OS_MAC */
---
+++ trunk/garglk/launchgtk.c
@@ -0,0 +1,164 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2006-2009 by Tor Andersson.                                  *
+ * Copyright (C) 2008-2010 by Ben Cressey.                                    *
+ * Copyright (C) 2009 by Baltasar GarcÌa Perez-Schofield.                     *
+ *                                                                            *
+ * This file is part of Gargoyle.                                             *
+ *                                                                            *
+ * Gargoyle is free software; you can redistribute it and/or modify           *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * Gargoyle is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with Gargoyle; if not, write to the Free Software                    *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA *
+ *                                                                            *
+ *****************************************************************************/
+
+#include "glk.h"
+#include "garversion.h"
+#include "launcher.h"
+
+#include <gtk/gtk.h>
+#include <unistd.h>
+
+static const char * AppName = "Gargoyle " VERSION;
+static const char * LaunchingTemplate = "%s/%s";
+static const char * DirSeparator = "/";
+
+#define MaxBuffer 1024
+char dir[MaxBuffer];
+char buf[MaxBuffer];
+char tmp[MaxBuffer];
+
+char filterlist[] = "";
+
+void wininit(void)
+{
+    gtk_init(NULL, NULL);
+}
+
+void winmsg(const char * msg)
+{
+    GtkWidget * msgDlg = gtk_message_dialog_new(NULL,
+                                         GTK_DIALOG_MODAL,
+                                         GTK_MESSAGE_ERROR,
+                                         GTK_BUTTONS_CLOSE,
+                                         "%s", msg
+    );
+
+    gtk_window_set_title(GTK_WINDOW(msgDlg), AppName);
+    gtk_dialog_run(GTK_DIALOG(msgDlg ));
+    gtk_widget_destroy(msgDlg);
+}
+
+bool winargs(int argc, char **argv, char *buffer)
+{
+    if (argc == 2)
+    {
+        strcpy(buffer, argv[1]);
+    }
+
+    return (argc == 2);
+}
+
+void winopenfile(char *buffer)
+{
+    *buffer = 0;
+
+    GtkWidget * openDlg = gtk_file_chooser_dialog_new(AppName, NULL,
+                                                      GTK_FILE_CHOOSER_ACTION_OPEN,
+                                                      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                                      GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                                      NULL);
+
+    if (getenv("HOME"))
+        gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(openDlg), getenv("HOME"));
+
+    gint result = gtk_dialog_run(GTK_DIALOG(openDlg));
+
+    if (result == GTK_RESPONSE_ACCEPT)
+        strcpy(buffer, gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(openDlg)));
+
+    gtk_widget_destroy(openDlg);
+    gdk_flush();
+}
+
+void winpath(char *buffer)
+{
+    char exepath[MaxBuffer] = {0};
+    unsigned int exelen;
+
+    exelen = readlink("/proc/self/exe", exepath, sizeof(exepath));
+
+    if (exelen <= 0 || exelen >= MaxBuffer) {
+        winmsg( "FATAL: Unable to locate executable path" );
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(buffer, exepath);
+
+    char *dirpos = strrchr(buffer, *DirSeparator);
+    if ( dirpos != NULL )
+        *dirpos = '\0';
+
+   return;
+}
+
+bool winexec(const char *cmd, char **args)
+{
+    return (execv(cmd, args) == 0);
+}
+
+void winterp(char *path, char *exe, char *flags, char *game)
+{
+    sprintf(tmp, LaunchingTemplate, path, exe);
+
+    char *args[] = {NULL, NULL, NULL};
+
+    if (strstr(flags, "-"))
+    {
+        args[0] = exe;
+        args[1] = flags;
+        args[2] = game;
+    }
+    else
+    {
+        args[0] = exe;
+        args[1] = buf;
+    }
+
+    if (!winexec(tmp, args)) {
+        winmsg("Could not start 'terp.\nSorry.");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+    wininit();
+
+    /* get dir of executable */
+    winpath(dir);
+
+    /* get story file */
+    if (!winargs(argc, argv, buf))
+        winopenfile(buf);
+
+    if (!strlen(buf))
+        exit(EXIT_SUCCESS);
+
+    /* run story file */
+    rungame(dir, buf);
+
+    return EXIT_FAILURE;
+}
