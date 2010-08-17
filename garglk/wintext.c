8f6ec63260986f59f3e1bdda54c45cb08a5c1eee
bcressey <ben@cressey.org>
2010-08-17 20:56:52 +0000

    Fix for issue #84 Blue Chairs, take two.
    svn-id: r389

--- garglk/wintext.c
+++ garglk/wintext.c
@@ -1070,6 +1070,8 @@ void win_textbuffer_clear(window_t *win)
     window_textbuffer_t *dwin = win->data;
     int i;
 
+    win->attr.fgset = gli_override_fg_set;
+    win->attr.bgset = gli_override_bg_set;
     win->attr.fgcolor = gli_override_fg_set ? gli_override_fg_val : 0;
     win->attr.bgcolor = gli_override_bg_set ? gli_override_bg_val : 0;
     win->attr.reverse = FALSE;
