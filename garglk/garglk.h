2fcf089c0074b016a02f93c2cdcf9e96d72cbac0
bcressey <ben@cressey.org>
2011-10-04 23:21:41 +0000

    Fix for issue #147 pulled from cheapglk, plus assorted fixes for reading and writing to Unicode file streams.
    svn-id: r566

--- garglk/garglk.h
+++ garglk/garglk.h
@@ -327,6 +327,7 @@ struct glk_stream_struct
 
     /* for strtype_File */
     FILE *file;
+    glui32 lastop; /* 0, filemode_Write, or filemode_Read */
     int textfile;
 
     /* for strtype_Memory */
