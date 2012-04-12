651400f840d04a03af7cf439eadb284227d5811f
bcressey <bcressey@be9d5a54-a115-11dd-a9c7-49dec45ca499>
2012-04-12 17:05:58 +0000

    Added 0.7.3 API functions to the null sound implementation. Resolves an issue when compiling with SDL support disabled.

--- trunk/garglk/sndnull.c
+++ trunk/garglk/sndnull.c
@@ -39,6 +39,11 @@ schanid_t glk_schannel_create(glui32 rock)
     return NULL;
 }
 
+schanid_t glk_schannel_create_ext(glui32 rock, glui32 volume)
+{
+    return NULL;
+}
+
 void glk_schannel_destroy(schanid_t chan)
 {
 }
@@ -69,14 +74,37 @@ glui32 glk_schannel_play_ext(schanid_t chan, glui32 snd, glui32 repeats,
     return 0;
 }
 
+glui32 glk_schannel_play_multi(schanid_t *chanarray, glui32 chancount,
+        glui32 *sndarray, glui32 soundcount, glui32 notify)
+{
+    gli_strict_warning("schannel_play_multi: invalid id.");
+    return 0;
+}
+
 void glk_schannel_stop(schanid_t chan)
 {
     gli_strict_warning("schannel_stop: invalid id.");
 }
 
+void glk_schannel_pause(schanid_t chan)
+{
+    gli_strict_warning("schannel_pause: invalid id.");
+}
+
+void glk_schannel_unpause(schanid_t chan)
+{
+    gli_strict_warning("schannel_unpause: invalid id.");
+}
+
 void glk_schannel_set_volume(schanid_t chan, glui32 vol)
 {
     gli_strict_warning("schannel_set_volume: invalid id.");
+}
+
+void glk_schannel_set_volume_ext(schanid_t chan, glui32 vol,
+        glui32 duration, glui32 notify)
+{
+    gli_strict_warning("schannel_set_volume_ext: invalid id.");
 }
 
 void glk_sound_load_hint(glui32 snd, glui32 flag)
