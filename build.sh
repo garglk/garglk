9a0f9abff82ebd1b1c57b214a61a63ba847e998f
poker335 <boneshaker335@yahoo.com>
2016-07-14 23:41:03 +0200

    compile options for linare gcc 5.3.1

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-export PATH=/home/schoen/dev/buildroot/buildroot-2015.11.1/output/host/usr/bin:$PATH
-#export PKG_CONFIG_DIR=/home/schoen/dev/buildroot/buildroot-2015.08/output/staging/usr/lib/pkgconfig
+export PATH=~/dev/buildroot/buildroot-2015.11.1/output/host/usr/bin:$PATH
+#export PKG_CONFIG_DIR=~/dev/buildroot/buildroot-2015.08/output/staging/usr/lib/pkgconfig
 
-SYSROOT=/home/schoen/dev/buildroot/buildroot-2015.11.1/output/staging
+SYSROOT=~/dev/buildroot/buildroot-2015.11.1/output/staging
 export PKG_CONFIG_DIR=
 export PKG_CONFIG_LIBDIR=${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig
 export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
 
-#jam -sBUILD=DEBUG -j6 -sCROSS=true
+#jam -sBUILD=DEBUG -j8 -sCROSS=true -sGARGLKINI=/mnt/us/extensions/gargoyle/garglk.ini
 #jam -sBUILD=DEBUG install
-jam -sCROSS=true -j6 -sGARGLKINI=/mnt/us/extensions/gdev/garglk.ini
-jam install
+#jam  -sCROSS=true -j8 -sGARGLKINI=/mnt/us/extensions/gdev/garglk.ini
+jam  -sCROSS=true -j8 -sGARGLKINI=/mnt/us/extensions/gargoyle/garglk.ini
+jam  install
