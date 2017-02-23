fd88a31baaf353659a6212fa4f92711e67bd9922
poker335 <boneshaker335@yahoo.com>
2017-02-23 13:27:14 +0100

    Cleanup.

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-export PATH=~/dev/buildroot/buildroot-2015.11.1/output/host/usr/bin:$PATH
-#export PKG_CONFIG_DIR=~/dev/buildroot/buildroot-2015.08/output/staging/usr/lib/pkgconfig
+export PATH=~/dev/buildroot/buildroot-2016.11.1/output/host/usr/bin:$PATH
 
-SYSROOT=~/dev/buildroot/buildroot-2015.11.1/output/staging
+SYSROOT=~/dev/buildroot/buildroot-2016.11.1/output/staging
 export PKG_CONFIG_DIR=
 export PKG_CONFIG_LIBDIR=${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig
 export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
 
-#jam -sBUILD=DEBUG -j8 -sCROSS=true -sGARGLKINI=/mnt/us/extensions/gargoyle/garglk.ini
+#jam -sBUILD=DEBUG 
 #jam -sBUILD=DEBUG install
-#jam  -sCROSS=true -j8 -sGARGLKINI=/mnt/us/extensions/gdev/garglk.ini
-jam  -sCROSS=true -j8 -sGARGLKINI=/mnt/us/extensions/gargoyle/garglk.ini
-jam  install
+jam  -dx -q -sCROSS=true -j3 -sGARGLKINI=/mnt/us/extensions/gargoyle/garglk.ini
+#jam  install
