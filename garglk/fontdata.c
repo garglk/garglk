c46e2b101b901365b20416bd6aaaafa1d1f7c7cc
Chris Spiegel <chris.spiegel@cypherpath.com>
2017-05-03 06:18:36 -0700

    Switch bundled fonts to Noto Serif and Go Mono.

--- garglk/fontdata.c
+++ garglk/fontdata.c
@@ -28,51 +28,51 @@
 #include "garglk.h"
 
 /* include hex-dumped font files */
-#include "lmr.hex"
-#include "lmb.hex"
-#include "lmi.hex"
-#include "lmz.hex"
-#include "cbr.hex"
-#include "cbb.hex"
-#include "cbi.hex"
-#include "cbz.hex"
+#include "Go-Mono.hex"
+#include "Go-Mono-Bold.hex"
+#include "Go-Mono-Italic.hex"
+#include "Go-Mono-Bold-Italic.hex"
+#include "NotoSerif-Regular.hex"
+#include "NotoSerif-Bold.hex"
+#include "NotoSerif-Italic.hex"
+#include "NotoSerif-BoldItalic.hex"
 
-void gli_get_builtin_font(int idx, unsigned char **ptr, unsigned int *len)
+void gli_get_builtin_font(int idx, const unsigned char **ptr, unsigned int *len)
 {
     switch (idx)
     {
         case 0:
-            *ptr = LuxiMonoRegular_pfb;
-            *len = LuxiMonoRegular_pfb_len;
+            *ptr = Go_Mono_ttf;
+            *len = Go_Mono_ttf_len;
             break;
         case 1:
-            *ptr = LuxiMonoBold_pfb;
-            *len = LuxiMonoBold_pfb_len;
+            *ptr = Go_Mono_Bold_ttf;
+            *len = Go_Mono_Bold_ttf_len;
             break;
         case 2:
-            *ptr = LuxiMonoOblique_pfb;
-            *len = LuxiMonoOblique_pfb_len;
+            *ptr = Go_Mono_Italic_ttf;
+            *len = Go_Mono_Italic_ttf_len;
             break;
         case 3:
-            *ptr = LuxiMonoBoldOblique_pfb;
-            *len = LuxiMonoBoldOblique_pfb_len;
+            *ptr = Go_Mono_Bold_Italic_ttf;
+            *len = Go_Mono_Bold_Italic_ttf_len;
             break;
 
         case 4:
-            *ptr = CharterBT_Roman_ttf;
-            *len = CharterBT_Roman_ttf_len;
+            *ptr = NotoSerif_Regular_ttf;
+            *len = NotoSerif_Regular_ttf_len;
             break;
         case 5:
-            *ptr = CharterBT_Bold_ttf;
-            *len = CharterBT_Bold_ttf_len;
+            *ptr = NotoSerif_Bold_ttf;
+            *len = NotoSerif_Bold_ttf_len;
             break;
         case 6:
-            *ptr = CharterBT_Italic_ttf;
-            *len = CharterBT_Italic_ttf_len;
+            *ptr = NotoSerif_Italic_ttf;
+            *len = NotoSerif_Italic_ttf_len;
             break;
         case 7:
-            *ptr = CharterBT_BoldItalic_ttf;
-            *len = CharterBT_BoldItalic_ttf_len;
+            *ptr = NotoSerif_BoldItalic_ttf;
+            *len = NotoSerif_BoldItalic_ttf_len;
             break;
 
         default:
