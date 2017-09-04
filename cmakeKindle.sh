c022937053d6563813d25975a5fd7caa2d28de6f
poker335 <boneshaker335@yahoo.com>
2017-09-04 17:36:29 +0200

    cmake for K*ndle build.

--- cmakeKindle.sh
+++ cmakeKindle.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-mkdir cbuildKindle
-cd buildKindle
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=dist -DWITH_SDL=off  -DCMAKE_TOOLCHAIN_FILE=/home/vagrant/dev/buildroot/buildroot-2016.11.1/output/host/usr/share/buildroot/toolchainfile.cmake ..
+
+CMAKE_BUILD_TYPE=Release
+
+if [ "$1" = "-d" ]
+then
+  CMAKE_BUILD_TYPE=Debug
+  echo "Enabling debug build."
+fi
+
+CMAKE_TOOLCHAIN_FILE=/home/vagrant/dev/buildroot/buildroot-2016.11.1/output/host/usr/share/buildroot/toolchainfile.cmake
+CMAKE_INSTALL_PREFIX=dist
+
+# Debug compile options
+#CMAKE_C_FLAGS_DEBUG="-g3 -gdwarf-2"
+CMAKE_C_FLAGS_DEBUG="-ggdb"
+CMAKE_CXX_FLAGS_DEBUG="-ggdb"
+
+# Kindle release build options
+CMAKE_C_FLAGS_RELEASE="-O3 -fomit-frame-pointer -ffast-math -fweb -frename-registers -fuse-linker-plugin -ftree-vectorize -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block"
+CMAKE_CXX_FLAGS_RELEASE="-O3 -fomit-frame-pointer -ffast-math -fweb -frename-registers -fuse-linker-plugin -ftree-vectorize -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block"
+
+mkdir -p cbuildKindle
+cd cbuildKindle
+
+cmake -G "Ninja"  -DWITH_SDL=off  \
+    -DCOMPILE_FOR_KINDLE=ON \
+    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" \
+    -DCMAKE_C_FLAGS="" \
+    -DCMAKE_CXX_FLAGS="" \
+    -DCMAKE_C_FLAGS_DEBUG="${CMAKE_C_FLAGS_DEBUG}" \
+    -DCMAKE_CXX_FLAGS_DEBUG="${CMAKE_CXX_FLAGS_DEBUG}" \
+    -DCMAKE_C_FLAGS_RELEASE="${CMAKE_C_FLAGS_RELEASE}" \
+    -DCMAKE_CXX_FLAGS_RELEASE="${CMAKE_CXX_FLAGS_RELEASE}" \
+    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE}" \
+    ..
 cd ..
-# -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-g3 -gdwarf-2" -DCMAKE_CXX_FLAGS_DEBUG="-g3 -gdwarf-2" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DWITH_SDL=off  -DCMAKE_TOOLCHAIN_FILE=/home/vagrant/dev/buildroot/buildroot-2016.11.1/output/host/usr/share/buildroot/toolchainfile.cmake ..
---
+++ cmakeLinux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CMAKE_BUILD_TYPE=Release
+
+if [ "$1" = "-d" ]
+then
+  CMAKE_BUILD_TYPE=Debug
+  echo "Enabling debug build."
+fi
+
+CMAKE_INSTALL_PREFIX=dist
+
+# Debug compile options
+CMAKE_C_FLAGS_DEBUG="-ggdb"
+CMAKE_CXX_FLAGS_DEBUG="-ggdb"
+
+# Kindle release build options
+#CMAKE_C_FLAGS_RELEASE="-O3 -fomit-frame-pointer -ffast-math -fweb -frename-registers -fuse-linker-plugin -ftree-vectorize -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block"
+#CMAKE_CXX_FLAGS_RELEASE="-O3 -fomit-frame-pointer -ffast-math -fweb -frename-registers -fuse-linker-plugin -ftree-vectorize -floop-interchange -ftree-loop-distribution -floop-strip-mine -floop-block"
+
+mkdir -p cbuildLinux
+cd cbuildLinux
+
+#-DCOMPILE_FOR_KINDLE=ON
+#-DCMAKE_C_FLAGS_DEBUG="${CMAKE_C_FLAGS_DEBUG}" 
+#-DCMAKE_CXX_FLAGS_DEBUG="${CMAKE_CXX_FLAGS_DEBUG}" 
+cmake -G "Ninja"  -DWITH_SDL=off  \
+  -DENABLE_ALT_MOUSE_HANDLING=ON \
+  -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+  -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" \
+  -DCMAKE_C_FLAGS_DEBUG="${CMAKE_C_FLAGS_DEBUG}" \
+  -DCMAKE_CXX_FLAGS_DEBUG="${CMAKE_CXX_FLAGS_DEBUG}" \
+  ..
+cd ..
