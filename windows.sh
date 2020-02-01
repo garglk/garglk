f1ecab0216bfcab92e68391faa6c8aa98917abf1
Chris Spiegel <cspiegel@gmail.com>
2020-02-01 06:43:19 -0800

    Update Windows build script.

--- windows.sh
+++ windows.sh
@@ -9,16 +9,15 @@ set -e
 
 [[ -e build/dist ]] && exit 1
 
-dlls="libstdc++-6.dll libgcc_s_sjlj-1.dll libwinpthread-1.dll"
 makensis="${HOME}/.wine/drive_c/Program Files (x86)/NSIS/makensis"
 nproc=$(getconf _NPROCESSORS_ONLN)
+ver=$(i686-w64-mingw32-gcc --version | head -1 | awk '{print $3}')
 
 jam -sC++=i686-w64-mingw32-g++ -sCC=i686-w64-mingw32-gcc -sOS=MINGW -sMINGWARCH=i686-w64-mingw32 -sCROSS=1 -sUSETTS=yes -dx -j${nproc}
 jam -sC++=i686-w64-mingw32-g++ -sCC=i686-w64-mingw32-gcc -sOS=MINGW -sMINGWARCH=i686-w64-mingw32 -sCROSS=1 -sUSETTS=yes -dx install
 
-for dll in ${dlls}
-do
-  cp "/usr/i686-w64-mingw32/bin/${dll}" build/dist
-done
+cp "/usr/lib/gcc/i686-w64-mingw32/${ver}/libstdc++-6.dll" "build/dist"
+cp "/usr/lib/gcc/i686-w64-mingw32/${ver}/libgcc_s_sjlj-1.dll" "build/dist"
+cp "/usr/i686-w64-mingw32/lib/libwinpthread-1.dll" "build/dist"
 
 wine "${makensis}" installer.nsi
