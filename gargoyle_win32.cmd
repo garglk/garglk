4cd4bddf850a35ac134c91a526b230a968327c47
Chris Spiegel <cspiegel@gmail.com>
2021-06-29 22:08:55 -0700

    Remove old Windows build scripts

--- gargoyle_win32.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-echo Packaging Gargoyle binaries ...
-jam -sOS=MINGW -sCC=gcc -sCXX=g++ -j%NUMBER_OF_PROCESSORS% install
-makensis.exe installer.nsi
-echo ... finished packaging!
-pause
-exit /b
