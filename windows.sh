21f9e0ddb7e3bbfb7e190036729eea60e0bd0d98
Chris Spiegel <cspiegel@gmail.com>
2021-07-09 08:13:58 -0700

    Support 64-bit Windows

--- windows.sh
+++ windows.sh
@@ -5,12 +5,20 @@
 # about the location of MinGW, so may need to be tweaked to get it to
 # properly work.
 
+if [[ -n "${GARGOYLE64}" ]]
+then
+    target=x86_64-w64-mingw32
+    libgcc=seh
+else
+    target=i686-w64-mingw32
+    libgcc=dw2
+fi
+
 set -e
 
 [[ -e build/dist ]] && exit 1
 
 mingw_location=/usr
-target=i686-w64-mingw32
 
 nproc=$(getconf _NPROCESSORS_ONLN)
 ver=$(${target}-gcc --version | head -1 | awk '{print $3}')
@@ -19,13 +27,13 @@ mkdir build-mingw
 
 (
 cd build-mingw
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw32.cmake -DMINGW_TRIPLE="${target}"
 make -j${nproc}
 make install
 )
 
 cp "/usr/lib/gcc/${target}/${ver}/libstdc++-6.dll" "build/dist"
-cp "/usr/lib/gcc/${target}/${ver}/libgcc_s_dw2-1.dll" "build/dist"
+cp "/usr/lib/gcc/${target}/${ver}/libgcc_s_${libgcc}-1.dll" "build/dist"
 cp "/usr/${target}/lib/libwinpthread-1.dll" "build/dist"
 
 for dll in Qt5Core Qt5Gui Qt5Widgets SDL2 SDL2_mixer libFLAC-8 libfreetype-6 libjpeg-8 libmodplug-1 libmpg123-0 libogg-0 libopenmpt-0 libpng16-16 libvorbis-0 libvorbisfile-3 zlib1
