fa7bbaf9f4c0366367790426bf23b0f3ff2f7c20
Chris Spiegel <cspiegel@gmail.com>
2021-10-30 06:24:16 -0700

    Update the RPM build script to use CMake

--- gargoyle-buildrpm.sh
+++ gargoyle-buildrpm.sh
@@ -5,6 +5,10 @@
 #  - rpm-build, make and gcc (as it's a c file) packages must be installed
 #
 
+set -e
+
+VERSION=$(<VERSION)
+
 # Holds the name of the root directory containing the necessary structure to
 # build RPM packages.
 RPM_ROOT_DIR=~/rpm_factory
@@ -12,25 +16,15 @@ PKG_NAME=gargoyle-free
 PKG_TAR=/tmp/${PKG_NAME}.tar.gz
 PKG_DIR=/tmp/${PKG_NAME}
 
-#Clean up any pre-existing build directories so we have a fresh build.
-if [ -d "./build/dist" ]
-then
-    rm -rfv ./build/dist
-fi
-if [ -d "./build/linux.release" ]
-then
-    rm -rfv ./build/linux.release
-fi
+#Build Gargoyle
+mkdir build-rpm
+pushd build-rpm
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+make -j$(nproc)
+make install DESTDIR=${PKG_DIR}
+popd
 
-#Build Gargoyle without SDL (currently causing issue building on Fedora)
-jam -sUSESDL=no
-jam -sUSESDL=no install
-
-#Create a directory to contain all elements of the distribution and copy them into it.
-mkdir -p ${PKG_DIR}
-cp -v ./build/dist/* ${PKG_DIR}
 cp -v ./garglk/garglk.ini ${PKG_DIR}
-cp -v ./garglk/gargoyle-house.png ${PKG_DIR}
 
 #Compress the package directory with all distribution elements in it as an input to RPM build process.
 pushd ${PKG_DIR}/..
@@ -46,7 +40,7 @@ cp ${PKG_TAR} ${RPM_ROOT_DIR}/SOURCES/
 cat << __EOF__ > ${RPM_ROOT_DIR}/SPECS/${PKG_NAME}.spec
 Summary: This package contains a build of the gargoyle interactive fiction interpreter.  This build is based on sources available at https://github.com/garglk/garglk
 Name: $PKG_NAME
-Version: 1.0
+Version: $VERSION
 Release: 0
 License: GPL
 Packager: interactivefiction
@@ -65,74 +59,58 @@ Provides: libgarglk.so()(64bit)
 %setup -n ${PKG_NAME}
 
 %build
-echo -e '#!/bin/sh\n%{_libdir}/gargoyle/gargoyle &\nexit' > gargoyle-free
-chmod +x gargoyle-free
-cat > %{name}.desktop <<'EOF'
-[Desktop Entry]
-Name=gargoyle-free
-Comment=Interactive fiction interpreter / player supporting many formats.
-Exec=gargoyle-free
-Icon=/usr/share/icons/gargoyle/gargoyle-house.png
-Categories=Game;Emulator;
-Type=Application
-Terminal=0
-X-Desktop-File-Install-Version=0.3
-EOF
 
 %install
+cp -av usr "%{buildroot}"
 mkdir -p "%{buildroot}%{_sysconfdir}/"
-mkdir -p "%{buildroot}%{_libdir}/gargoyle/"
-mkdir -p "%{buildroot}%{_bindir}/"
-mkdir -p "%{buildroot}%{_datadir}/applications/"
-mkdir -p "%{buildroot}%{_datadir}/icons/gargoyle/"
 cp -v garglk.ini "%{buildroot}%{_sysconfdir}/"
-cp -v libgarglk.so "%{buildroot}%{_libdir}/"
-cp -v advsys "%{buildroot}%{_libdir}/gargoyle/"
-cp -v geas "%{buildroot}%{_libdir}/gargoyle/"
-cp -v magnetic "%{buildroot}%{_libdir}/gargoyle/"
-cp -v agility "%{buildroot}%{_libdir}/gargoyle/"
-cp -v git "%{buildroot}%{_libdir}/gargoyle/"
-cp -v nitfol "%{buildroot}%{_libdir}/gargoyle/"
-cp -v alan2 "%{buildroot}%{_libdir}/gargoyle/"
-cp -v glulxe "%{buildroot}%{_libdir}/gargoyle/"
-cp -v scare "%{buildroot}%{_libdir}/gargoyle/"
-cp -v alan3 "%{buildroot}%{_libdir}/gargoyle/"
-cp -v hugo "%{buildroot}%{_libdir}/gargoyle/"
-cp -v scott "%{buildroot}%{_libdir}/gargoyle/"
-cp -v bocfel "%{buildroot}%{_libdir}/gargoyle/"
-cp -v jacl "%{buildroot}%{_libdir}/gargoyle/"
-cp -v tadsr "%{buildroot}%{_libdir}/gargoyle/"
-cp -v frotz "%{buildroot}%{_libdir}/gargoyle/"
-cp -v level9 "%{buildroot}%{_libdir}/gargoyle/"
-cp -v gargoyle "%{buildroot}%{_libdir}/gargoyle/"
-cp -v gargoyle-free "%{buildroot}%{_bindir}/"
-cp -v %{name}.desktop "%{buildroot}%{_datadir}/applications/"
-cp -v gargoyle-house.png "%{buildroot}%{_datadir}/icons/gargoyle/" 
 
 %files
 %{_sysconfdir}/garglk.ini
 %{_libdir}/libgarglk.so
-%{_libdir}/gargoyle/advsys
-%{_libdir}/gargoyle/geas
-%{_libdir}/gargoyle/magnetic
-%{_libdir}/gargoyle/agility
-%{_libdir}/gargoyle/git
-%{_libdir}/gargoyle/nitfol
-%{_libdir}/gargoyle/alan2
-%{_libdir}/gargoyle/glulxe
-%{_libdir}/gargoyle/scare
-%{_libdir}/gargoyle/alan3
-%{_libdir}/gargoyle/hugo
-%{_libdir}/gargoyle/scott
-%{_libdir}/gargoyle/bocfel
-%{_libdir}/gargoyle/jacl
-%{_libdir}/gargoyle/tadsr
-%{_libdir}/gargoyle/frotz
-%{_libdir}/gargoyle/level9
-%{_libdir}/gargoyle/gargoyle
-%{_bindir}/gargoyle-free
-%{_datadir}/applications/%{name}.desktop
-%{_datadir}/icons/gargoyle/gargoyle-house.png
+%{_libdir}/libgarglkmain.a
+%{_libexecdir}/gargoyle/advsys
+%{_libexecdir}/gargoyle/geas
+%{_libexecdir}/gargoyle/magnetic
+%{_libexecdir}/gargoyle/agility
+%{_libexecdir}/gargoyle/git
+%{_libexecdir}/gargoyle/alan2
+%{_libexecdir}/gargoyle/glulxe
+%{_libexecdir}/gargoyle/scare
+%{_libexecdir}/gargoyle/alan3
+%{_libexecdir}/gargoyle/hugo
+%{_libexecdir}/gargoyle/scott
+%{_libexecdir}/gargoyle/bocfel
+%{_libexecdir}/gargoyle/jacl
+%{_libexecdir}/gargoyle/tadsr
+%{_libexecdir}/gargoyle/level9
+%{_bindir}/gargoyle
+%{_includedir}/garglk/gi_blorb.h
+%{_includedir}/garglk/glk.h
+%{_includedir}/garglk/glkstart.h
+%{_libdir}/pkgconfig/garglk.pc
+%{_datarootdir}/applications/gargoyle.desktop
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Mono-Bold-Italic.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Mono-Bold.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Mono-Italic.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Mono.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Serif-Bold-Italic.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Serif-Bold.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Serif-Italic.ttf
+%{_datarootdir}/fonts/gargoyle/Gargoyle-Serif.ttf
+%{_datarootdir}/icons/gargoyle-house.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-adrift.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-advsys.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-agt.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-alan.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-blorb.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-glulx.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-hugo-image.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-level9.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-magscroll.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-tads.png
+%{_datarootdir}/icons/hicolor/32x32/mimetypes/application-x-zmachine.png
+%{_datarootdir}/mime/packages/interactive-fiction.xml
 
 %clean
 %if "%{clean}" != ""
@@ -140,7 +118,6 @@ cp -v gargoyle-house.png "%{buildroot}%{_datadir}/icons/gargoyle/"
 %endif
 
 %post
-chmod 755 -R /opt/${PKG_NAME}
 __EOF__
 
 rpmbuild -v -bb --define "_topdir ${RPM_ROOT_DIR}" SPECS/${PKG_NAME}.spec
