86100023b5bfdb87a7dea8caf810fc2eca54e0f7
Chris Spiegel <cspiegel@gmail.com>
2021-12-04 06:51:39 -0800

    Don't use gnu_printf on Clang, as it doesn't exist there

--- terps/bocfel/util.h
+++ terps/bocfel/util.h
@@ -11,7 +11,7 @@
 
 #if defined(__GNUC__) && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7))
 #define znoreturn		__attribute__((__noreturn__))
-#ifdef __MINGW32__
+#if defined(__MINGW32__) && defined(__GNUC__) && !defined(__clang__)
 // Gcc appears to default to ms_printf on MinGW, even when it is
 // providing standards-conforming printf() functionality (i.e. if
 // __USE_MINGW_ANSI_STDIO is defined), so force gnu_printf there.
