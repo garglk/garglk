dc12b8d88af6d1bd1f3cfa098a99c5312f884a1f
angstsmurf <ignalina@me.com>
2022-04-08 21:40:30 +0200

    SCOTT: Print the correct variable

--- terps/scott/detectgame.c
+++ terps/scott/detectgame.c
@@ -83,7 +83,7 @@ void PrintHeaderInfo(int *h, int ni, int na, int nw, int nr, int mc, int pr, int
     fprintf(stderr, "Word length =\t%d\n", wl);
     fprintf(stderr, "Number of messages =\t%d\n", mn);
     fprintf(stderr, "Player start location: %d\n", pr);
-    fprintf(stderr, "Treasure room: %d\n", tr);
+    fprintf(stderr, "Treasure room: %d\n", trm);
     fprintf(stderr, "Lightsource time left: %d\n", lt);
     fprintf(stderr, "Number of treasures: %d\n", tr);
 }
