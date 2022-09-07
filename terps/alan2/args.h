91a0723e2d272da71bb9753212b6ccdb2a5474cb
Chris Spiegel <cspiegel@gmail.com>
2022-09-07 14:39:47 -0700

    Ensure function declarations are prototypes
    Clang 15 introduces a warning regarding non-prototype declarations (i.e.
    those with empty parentheses) in advance of C2X finally ditching K&R
    declarations. There are a few interpreters which still have K&R
    declarations, so fix those. These are still built with C11, so K&R
    declarations are valid, if awful, but it's still good practice to use
    proper prototypes.
    
    JACL still has a _lot_ of K&R declarations, but I'm not changing those
    because upstream already has this fixed, so next upstream update will
    fix these issues.

--- terps/alan2/args.h
+++ terps/alan2/args.h
@@ -29,6 +29,13 @@ extern BPTR cd;
 #define PROGNAME "alan2"
 #endif
 
+// _PROTOTYPES_ (which is a reserved identifier, but oh well) is defined in
+// sysdep.h, so theoretically that file should be included here; but that
+// causes, eventually, a conflicting definition of printf, as Alan defines
+// printf as a macro to a function that's _not_ compatible with the actual
+// printf(). Instead of fixing that, just define _PROTOTYPES_ here.
+#define _PROTOTYPES_
+
 #ifdef _PROTOTYPES_
 extern void args(int argc, char *argv[]);
 #else
