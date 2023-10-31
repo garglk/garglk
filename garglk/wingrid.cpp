8f94a1b820d1745a5c34a3d1c2e74d00b0760bd1
Adrian Welcker <ardi@diepixelecke.de>
2023-10-31 21:54:06 +0100

    Nicer std::fill fix

--- garglk/wingrid.cpp
+++ garglk/wingrid.cpp
@@ -77,11 +77,7 @@ void win_textgrid_rearrange(window_t *win, rect_t *box)
 
     for (k = 0; k < dwin->height; k++) {
         touch(dwin, k);
-        // std::fill(&dwin->lines[k].chars[dwin->width], dwin->lines[k].chars.end(), ' ');
-        auto *end = (&dwin->lines[k].chars[0]) + dwin->lines[k].chars.size();
-        for (auto *x = &dwin->lines[k].chars[dwin->width]; x < end; x++) {
-            *x = ' ';
-        }
+        std::fill(dwin->lines[k].chars.begin() + dwin->width, dwin->lines[k].chars.end(), ' ');
         auto *attr_end = (&dwin->lines[k].attrs[0]) + dwin->lines[k].attrs.size();
         for (auto *attr = &dwin->lines[k].attrs[dwin->width]; attr < attr_end; ++attr) {
             attr->clear();
