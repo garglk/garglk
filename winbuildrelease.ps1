c77406588e6cbecc7e1609ff758fe3a675282a03
Adrian Welcker <ardi@diepixelecke.de>
2024-09-06 16:28:58 +0200

    Update Windows build script for dotnet-via-CMake

--- winbuildrelease.ps1
+++ winbuildrelease.ps1
@@ -6,8 +6,7 @@
     [Parameter(Mandatory=$true)]
     [string]$vcpkgPath,
     [Parameter(Mandatory=$true)]
-    [string]$qtPath,
-    [bool]$arm64Cross = $false
+    [string]$qtPath
 )
 
 $ErrorActionPreference = "Stop"
@@ -26,11 +25,8 @@ if ($withFrankendrift) {
 	$cmFd = "OFF"
 }
 
-if ($arm64Cross) {
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_LINKER=lld-link -DINTERFACE=QT -DWITH_QT6=ON -DCMAKE_PREFIX_PATH="$qtPath/msvc2019_arm64" -DCMAKE_TOOLCHAIN_FILE="$vcpkgPath/buildsystems/vcpkg.cmake" -DSOUND=QT -DWITH_FRANKENDRIFT=$cmFd -DCMAKE_C_COMPILER_TARGET=arm64-pc-windows-msvc -DCMAKE_CXX_COMPILER_TARGET=arm64-pc-windows-msvc -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY -DCMAKE_CROSSCOMPILING=ON -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_PROCESSOR=arm64 -DCMAKE_STATIC_LINKER_FLAGS="/machine:arm64 " -DCMAKE_EXE_LINKER_FLAGS="/machine:arm64 " ..
-} else {
-    cmake -G Ninja -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_LINKER=lld-link -DINTERFACE=QT -DCMAKE_PREFIX_PATH="$qtPath/msvc2019_64" -DCMAKE_TOOLCHAIN_FILE="$vcpkgPath/scripts/buildsystems/vcpkg.cmake" -DSOUND=QT -DWITH_FRANKENDRIFT=$cmFd ..
-}
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE="$buildType" -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_LINKER=lld-link -DINTERFACE=QT -DCMAKE_PREFIX_PATH="$qtPath/msvc2019_64" -DCMAKE_TOOLCHAIN_FILE="$vcpkgPath/scripts/buildsystems/vcpkg.cmake" -DSOUND=QT -DDIST_INSTALL=ON -DWITH_FRANKENDRIFT=$cmFd ..
 ninja
 
 $stagedir = "$PSScriptRoot\gargoyle-staging"
@@ -52,22 +48,9 @@ if ($withPdbs) {
 }
 
 if ($withFrankendrift) {
-    mkdir "$builddir\terps\frankendrift"
-    cd "$PSScriptRoot\terps\frankendrift\FrankenDrift.GlkRunner\FrankenDrift.GlkRunner.Gargoyle"
-    Copy-Item "$builddir\garglk\garglk.lib" .
-    if ($buildType -eq "Debug") {
-        $fdConfig = "Debug"
-    } else {
-        $fdConfig = "Release"
-    }
-    if ($arm64Cross) {
-        dotnet publish --self-contained -c $fdConfig -f net8.0 -r win-arm64 -p:GarglkStatic=true -o "$builddir\terps\frankendrift"
-    } else {
-        dotnet publish --self-contained -c $fdConfig -f net8.0 -r win-x64 -p:GarglkStatic=true -o "$builddir\terps\frankendrift"
-    }
-    Copy-Item $builddir\terps\frankendrift\FrankenDrift.GlkRunner.Gargoyle.exe "$stagedir"
+    Copy-Item "$PSScriptRoot\terps\frankendrift\FrankenDrift.GlkRunner\FrankenDrift.GlkRunner.Gargoyle\FrankenDrift.GlkRunner.Gargoyle.exe" "$stagedir"
     if ($withPdbs) {
-        Copy-Item "$builddir\terps\frankendrift\*.pdb" "$stagedir"
+        Copy-Item "$PSScriptRoot\terps\frankendrift\FrankenDrift.GlkRunner\FrankenDrift.GlkRunner.Gargoyle\*.pdb" "$stagedir"
     }
 }
 
