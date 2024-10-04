d9bee87cadf11648ec40249c4b0679a0b700e2a0
Adrian Welcker <ardi@diepixelecke.de>
2024-10-04 08:29:50 +0200

    Switch to generated regex

--- terps/frankendrift/FrankenDrift.Glue/Util.cs
+++ terps/frankendrift/FrankenDrift.Glue/Util.cs
@@ -4,15 +4,14 @@ using FrankenDrift.Glue.Infragistics.Win.UltraWinToolbars;
 
 namespace FrankenDrift.Glue
 {
-    public static class Util
+    public static partial class Util
     {
         public static string StripCarats(string str) => StripCarets(str);
         public static string StripCarets(string str)
         {
             if (str.Contains("<#") && str.Contains("#>"))
                 str = str.Replace("<#", "[[==~~").Replace("#>", "~~==]]");
-            var re = new Regex("<(.|\n)+?>");
-            return re.Replace(str, "");
+            return HtmlTagRegex().Replace(str, "");
         }
 
         public static string GetSetting(string _a, string _b, string _c, string _d = "") => _d;
@@ -21,6 +20,9 @@ namespace FrankenDrift.Glue
 
         public static void AddPrevious() { }
         public static void AddPrevious(UltraToolbarsManager _a, string _b) { }
+
+        [GeneratedRegex("<(.|\n)+?>")]
+        private static partial Regex HtmlTagRegex();
     }
 
     public static class Application
