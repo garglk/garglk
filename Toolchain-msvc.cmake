4c0b2225623fb04fc2a0e37ebf399803656e574f
Chris Spiegel <cspiegel@gmail.com>
2026-02-18 20:08:42 -0800

    Use standard LLVM tools for MSVC cross-compilation
    Replace custom compiler wrappers with clang-cl, lld-link, llvm-mt, and
    llvm-rc. This makes the toolchain file self-contained and reproducible
    without requiring custom wrapper scripts.

--- Toolchain-msvc.cmake
+++ Toolchain-msvc.cmake
@@ -1,18 +1,27 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR X86_64)
 
-set(CMAKE_BUILD_TYPE "Release")
-set(CMAKE_C_COMPILER "x86_64-pc-windows-msvc-gcc")
-set(CMAKE_CXX_COMPILER "x86_64-pc-windows-msvc-g++")
-set(CMAKE_EXE_LINKER_FLAGS "/winsdkdir:/usr/x86_64-pc-windows-msvc/winsdk/sdk /vctoolsdir:/usr/x86_64-pc-windows-msvc/winsdk/crt /libpath:/usr/x86_64-pc-windows-msvc/winsdk/sdk/lib/um/x64")
-set(CMAKE_SHARED_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS})
+set(CMAKE_C_COMPILER "clang-cl")
+set(CMAKE_CXX_COMPILER "clang-cl")
+set(CMAKE_LINKER "lld-link")
 set(CMAKE_MT "llvm-mt")
+set(CMAKE_RC_COMPILER "llvm-rc")
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-pc-windows-msvc)
+set(MSVC_SYSROOT "/usr/x86_64-pc-windows-msvc")
+set(WINSDK "${MSVC_SYSROOT}/winsdk")
+
+set(CMAKE_C_FLAGS_INIT "-D_CRT_SECURE_NO_WARNINGS -fuse-ld=lld-link /vctoolsdir \"${WINSDK}/crt\" /winsdkdir \"${WINSDK}/sdk\"")
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} /EHsc")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "/winsdkdir:${WINSDK}/sdk /vctoolsdir:${WINSDK}/crt /libpath:${WINSDK}/sdk/lib/um/x64")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT}")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT}")
+
+set(CMAKE_FIND_ROOT_PATH "${MSVC_SYSROOT}")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(ENV{PKG_CONFIG_DIR} "")
-set(ENV{PKG_CONFIG_LIBDIR} "/usr/x86_64-pc-windows-msvc/lib/pkgconfig")
+set(ENV{PKG_CONFIG_LIBDIR} "${MSVC_SYSROOT}/lib/pkgconfig")
