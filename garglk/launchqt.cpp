5504a50dc1df5a620e89351863bb04e477b60d48
Dan Fabulich <dan@fabulich.com>
2026-08-10 21:13:10 -0700

    Basic menu bar

--- garglk/launchqt.cpp
+++ garglk/launchqt.cpp
@@ -41,6 +41,7 @@
 #include <QList>
 #include <QMessageBox>
 #include <QProcess>
+#include <QProcessEnvironment>
 #include <QPushButton>
 #include <QStandardPaths>
 #include <QString>
@@ -156,6 +157,12 @@ bool garglk::winterp(const std::string &exe, const std::vector<std::string> &fla
 
     QProcess proc;
     proc.setProcessChannelMode(QProcess::ForwardedChannels);
+
+    // So interpreters can re-launch Gargoyle (File → Open / Open Recent).
+    auto env = QProcessEnvironment::systemEnvironment();
+    env.insert("GARGLK_LAUNCHER", QCoreApplication::applicationFilePath());
+    proc.setProcessEnvironment(env);
+
     proc.start(argv0, args);
 
     if (!proc.waitForStarted(5000)) {
